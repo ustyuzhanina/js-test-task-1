ed8425c135c54b5019b66962411abea35626c205
ustyuzhanina <elena.ustyuzhanina.rus@gmail.com>
2021-03-23 23:39:59 +0700

    ready function

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 // write here
-function getRelevantPlace(places) {
+const pack1 = require('./data/pack1.js');
+const pack2 = require('./data/pack2.js');
+const pack3 = require('./data/pack3.js');
 
+function getRelevantPlace(places) {
+  places.sort((a, b) => {
+      return a.peopleCount - b.peopleCount;
+  });
+
+  const equalQtyArray = [];
+  places.forEach(item => {      
+      if (item.peopleCount === places[0]['peopleCount']) {
+        equalQtyArray.push(item);
+      }
+  })
+
+  if (equalQtyArray.length > 1) {
+      const index = Math.floor(Math.random() * equalQtyArray.length);
+      return equalQtyArray[index]['id'];
+  } else {
+    return equalQtyArray[0]['id'];
+  }
+  
 }
 
 module.exports = {
